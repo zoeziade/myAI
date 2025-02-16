a510303a19f3510fb0590420d2712651e9a3c153
Daniel Ringel <33063149+dringel@users.noreply.github.com>
2025-02-16 10:36:47 -0500

    Update footer.tsx

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -7,3 +7,17 @@ export default function ChatFooter() {
     </div>
   );
 }
+
+import { FOOTER_MESSAGE } from "@/app/configuration/ui";
+
+export default function ChatFooter() {
+  return (
+    <div className="w-full text-xs flex pt-2 justify-center items-center text-gray-500 text-center">
+      {FOOTER_MESSAGE} 
+      <span className="ml-1">
+        {/* Do not remove or modify the following message. Removal or modification violates the license agreement. */}
+        powered by ringel.AI
+      </span>
+    </div>
+  );
+}
