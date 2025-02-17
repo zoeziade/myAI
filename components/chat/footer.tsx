94c0f400e46d898c14097b0b24c4ed62672f269f
Farouk Charkas <farouk.charkas@gmail.com>
2025-02-17 11:04:12 -0500

    Add link to terms

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -1,9 +1,15 @@
 import { FOOTER_MESSAGE } from "@/configuration/ui";
+import Link from "next/link";
 
 export default function ChatFooter() {
   return (
     <div className="w-full text-xs flex pt-2 text-gray-500">
-      <div className="flex-grow text-left">{/* Left Pane */}</div>
+      <div className="flex-grow text-left">
+        {/* Left Pane */}
+        <Link href="/terms" className="hover:underline">
+          Terms of Service
+        </Link>
+      </div>
       <div className="flex-grow text-center">
         {/* Center Pane */}
         {FOOTER_MESSAGE}
