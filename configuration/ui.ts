0c8c47be9ab65591d535435f391ddf2823917eae
zoeziade <zoeziade@gmail.com>
2025-02-20 12:56:18 -0500

    Changed footer text to my own

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
+export const FOOTER_MESSAGE: string = `Customized AI by Zoe Z @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
