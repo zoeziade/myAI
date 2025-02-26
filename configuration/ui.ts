8c5063689926eb862e9189fe901c3136b4ce9c37
zoeziade <zoeziade@gmail.com>
2025-02-26 18:08:49 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `BizGenius: AI assistant for entrepreneurs`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Zoe Z @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear chat`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `BizGenius`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
