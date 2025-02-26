6626dd0fafeb046c05180d2a35d556bbad6d396a
zoeziade <zoeziade@gmail.com>
2025-02-26 14:30:54 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[Zoe Ziade]`;
+export const OWNER_NAME: string = `Zoe Ziade`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[Stewie]`;
+export const AI_NAME: string = `Stewie`;
 export const AI_TONE: string = `[Helpful, knowledgable, and patient]`;
 export const AI_ROLE: string = `[Assistant to future entrepreneurs]`;
