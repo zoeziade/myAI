fc491fcc9a766c72566c475b148c519ef8a86417
zoeziade <zoeziade@gmail.com>
2025-03-17 16:05:22 -0400

    Update citation.tsx

--- components/chat/citation.tsx
+++ components/chat/citation.tsx
@@ -2,11 +2,7 @@
 
 import { useState } from "react";
 import { Citation } from "@/types";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@radix-ui/react-tooltip";
+import { Tooltip } from "react-tippy";
 import Link from "next/link";
 import { EMPTY_CITATION_MESSAGE } from "@/configuration/ui";
 
@@ -31,16 +27,15 @@ export function CitationCircle({
   const hasSourceDescription = citation.source_description.trim() !== "";
 
   return (
-    <Tooltip open={open} onOpenChange={setOpen}>
-      <TooltipTrigger>
-        <div
-          className="bg-blue-200 rounded-full px-2 py-0.5 hover:cursor-pointer hover:scale-105 inline-block"
-          onClick={() => setOpen(true)}
-        >
-          <span>{number}</span>
-        </div>
-      </TooltipTrigger>
-      <TooltipContent>
+    <Tooltip
+      title=""
+      open={open}
+      onRequestClose={() => setOpen(false)}
+      position="bottom"
+      // @ts-expect-error tippy docs allow this
+      trigger="mouseenter click"
+      interactive={true}
+      html={
         <div className="bg-white p-2 rounded-md shadow-sm flex flex-col justify-center border-[1px] border-gray-200">
           <p>
             {hasSourceUrl && (
@@ -56,7 +51,14 @@ export function CitationCircle({
             {!hasSourceUrl && !hasSourceDescription && EMPTY_CITATION_MESSAGE}
           </p>
         </div>
-      </TooltipContent>
+      }
+    >
+      <div
+        className="bg-gray-50 rounded-full px-2 py-0.5 hover:cursor-pointer hover:scale-105 inline-block"
+        onClick={() => setOpen(true)}
+      >
+        <span>{number}</span>
+      </div>
     </Tooltip>
   );
 }
